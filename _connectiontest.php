6d40321765a19d0dfa47706e7c1a6976937e6acd
Nasir Ahamed <m.nasir.ahamed@gmail.com>
2016-02-27 18:09:07 +0000

    connectiontest file updated

--- _connectiontest.php
+++ _connectiontest.php
@@ -16,5 +16,19 @@ echo "FREEZE APP Database Connection Successfull";
 
 ?>
 
-<!--To check database conneciton copy and past below link in your browswer.-->
-<!--https://freezeapp-nasirahamed-2.c9users.io/_connectiontest.php-->
+<!--
+To check database conneciton copy and past below link in your browswer:
+https://freezeapp-nasirahamed-2.c9users.io/_connectiontest.php
+
+
+To login to PhpMyAdmin, use the following details:
+https://freezeapp-nasirahamed-1.c9users.io/phpmyadmin
+
+UserName:
+root
+
+Password:
+root
+
+After login select the freezeapp database.
+-->
