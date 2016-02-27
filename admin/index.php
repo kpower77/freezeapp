47514d7d0072472a5622866bf53906b69a4b283a
Nasir Ahamed <m.nasir.ahamed@gmail.com>
2016-02-27 13:40:33 +0000

    I have received all directories and I will shortly begin coding

---
+++ admin/index.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+			<title></title>
+	</head>
+<body>
+	
+</body>
+</html>
+<?php include("includes/header.php") ?>
+
+  
+  <?php include("includes/nav.php") ?>
+
+
+
+
+	<div class="jumbotron">
+
+		<?php display_message(); ?>
+		<h1 class="text-center"> Home </h1>
+	</div>
+
+
+<?php include("includes/footer.php") ?>
