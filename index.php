23e3b287e4195dc0404ed57d276b5ffa07feefab
Nasir Ahamed <m.nasir.ahamed@gmail.com>
2016-02-27 15:17:12 +0000

    All login system files uploaded

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+	<head>
+	<meta charset="UTF-8">
+	<title>Freeze App</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<script src="js/scripts.js"></script>
+	</head>
+<body>
+	
+</body>
+</html>
+<?php include("includes/header.php") ?>
+
+  
+  <?php include("includes/nav.php") ?>
+
+
+
+
+	<div class="jumbotron">
+
+		<?php display_message(); ?>
+		<h1 class="text-center"> Home </h1>
+	</div>
+
+
+<?php include("includes/footer.php") ?>
