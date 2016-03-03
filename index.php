46fdfd3179ea687e04a68dc251016b25d4b3a9f3
Nasir Ahamed <m.nasir.ahamed@gmail.com>
2016-03-03 19:07:55 +0000

    Index.php file updated

--- index.php
+++ index.php
@@ -1,28 +1,7 @@
-<!DOCTYPE html>
-<html>
-	<?php include("includes/header.php") ?>
-
+<?php include("includes/header.php") ?>
 <?php include("includes/nav.php") ?>
-
-<<<<<<< HEAD
-	<div class="jumbotron">
-=======
-<html>
-	<head>
-		
-	</head>
-	
-	<body>
-		<div class="jumbotron">
-
-
-		<?php //display_message(); ?>
-		<h1 class="text-center"> Home - Main </h1>
-		</div>	
-		
-	</body>
-	
-</html>
-
-	<?php include("includes/footer.php") ?>
-</html>
+<div class="jumbotron">
+	<?php //display_message(); ?>
+	<h1 class="text-center"> Home - Main </h1>
+</div>	
+<?php include("includes/footer.php") ?>
