fdc3401bac2ceaf4fbd9e269e571f412215280c6
Nasir Ahamed <nasir.ahamed@me.com>
2016-03-08 13:46:46 +0000

    Merging with local

---
+++ activate.php
@@ -0,0 +1,15 @@
+<?php include("includes/header.php") ?>
+
+  
+  <?php include("includes/nav.php") ?>
+
+
+
+
+	<div class="jumbotron">
+		<h1 class="text-center">Account Activated</h1>
+	</div>
+
+
+	
+<?php include("includes/footer.php") ?>
