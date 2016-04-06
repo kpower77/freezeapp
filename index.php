61d475a9f813f9189d74f60b9947754c0515a03c
x14114640 <florinbest90@yahoo.com>
2016-04-06 17:53:48 +0000

    Rearraged the php code and removed some of database errors

--- index.php
+++ index.php
@@ -1,25 +1,9 @@
 <?php include("includes/header.php") ?>
 <?php include("includes/nav.php") ?>
 
-<div class="jumbotron">
-	<?php //display_message(); ?>
-	<h1 class="text-center"> SyncroSoft</h1>
-</div>
-
-
-<?php 
-
-$sql = "SELECT * FROM users";
-$result = query($sql);
-
-confirm($result);
-
-$row = fetch_array($result);
-
-echo $row['username']; //pulling username from database
-
-
-?>
-
-
 <?php include("includes/footer.php") ?>
+
+<!DOCTYPE html>
+<div class="jumbotron">
+	<h1 class="text-center"></h1>
+</div>
