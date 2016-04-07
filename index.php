9d28599d29d6b792347d5161e5bd24fbe4c162f7
x14114640 <florinbest90@yahoo.com>
2016-04-07 12:15:30 +0000

    rearranged files and routes, still need to correctly assign Jscript and CSS

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 <?php include("includes/nav.php") ?>
 
 <!DOCTYPE html>
-<div class="jumbotron">
-	<h1 class="text-center">FreezeApp</h1>
-	<!-- Apply blue theme as default for all tiles, but use the tiles class to allow the theme to be swapped -->
-    <div class="tiles blue tile-group four-wide">
+<html>
+<head>
+</head>
+<body>
+    <div class="jumbotron">
+    	<h1 class="text-center">FreezeApp</h1>
+    	    <!-- Apply blue theme as default for all tiles, but use the tiles class to allow the theme to be swapped -->
+        <div class="tiles blue tile-group four-wide">
         <!-- Sliding Tile that shows 100% of the back tile every 3 seconds -->
         <div class="live-tile" data-speed="750" data-delay="3000">
             <span class="tile-title">slide tile</span>
@@ -68,14 +72,89 @@
         <div class="live-tile accent green exclude">
             <span class="tile-title">static tile</span>
             <p>Static tiles can take advantage of theming too!</p>
-        </div>
     </div>
+</div>
+<!-- Activate live tiles -->
+<script type="text/javascript">
+    // apply regular slide universally unless .exclude class is applied 
+    // NOTE: The default options for each liveTile are being pulled from the 'data-' attributes
+    $(".live-tile, .flip-list").not(".exclude").liveTile();
+</script>
+    </div>
+</body>
+    
+</html>
+<?php include("includes/footer.php") ?>
+
+  <!--Apply blue theme as default for all tiles, but use the tiles class to allow the theme to be swapped -->
+  <!--  <div class="tiles blue tile-group four-wide">-->
+        <!-- Sliding Tile that shows 100% of the back tile every 3 seconds -->
+  <!--      <div class="live-tile" data-speed="750" data-delay="3000">-->
+  <!--          <span class="tile-title">slide tile</span>-->
+  <!--          <div><img class="full" src="/images/sample/1tw.gif" alt="1" /></div>-->
+  <!--          <div><img class="full" src="/images/sample/2t.gif" alt="2" /></div>-->
+  <!--      </div>-->
+        <!-- Red Flip Tile that flips every 4 seconds -->
+  <!--      <div class="red live-tile" data-mode="flip" data-delay="4000">            -->
+  <!--          <div>-->
+  <!--              <img class="full" src="/images/sample/3tw.gif" alt="3" />-->
+  <!--              <span class="tile-title">flip tile front</span>-->
+  <!--          </div>-->
+  <!--          <div>-->
+  <!--              <img class="full" src="/images/sample/4tw.gif" alt="4" />-->
+  <!--              <span class="tile-title">flip tile back</span>-->
+  <!--          </div>-->
+  <!--      </div>-->
+        <!-- Carousel Tile that slides 4 tile faces every 2.5 seconds -->
+  <!--      <div class="live-tile" data-mode="carousel" data-direction="horizontal" data-delay="2500">-->
+  <!--          <div>-->
+  <!--              <img class="full" src="/images/sample/1tw.gif" alt="1" />-->
+  <!--              <span class="tile-title">carousel slide 1</span>-->
+  <!--          </div>-->
+  <!--          <div>-->
+  <!--              <img class="full" src="/images/sample/2t.gif" alt="2" />-->
+  <!--              <span class="tile-title accent">carousel slide 2</span>-->
+  <!--          </div>-->
+  <!--          <div>-->
+  <!--              <img class="full" src="/images/sample/3tw.gif" alt="3" />-->
+  <!--              <span class="tile-title">carousel slide 3</span>-->
+  <!--          </div>-->
+  <!--          <div>-->
+  <!--              <img class="full" src="/images/sample/4t.gif" alt="4" />-->
+  <!--              <span class="tile-title accent">carousel slide 4</span>-->
+  <!--          </div>-->
+  <!--      </div>-->
+        <!-- Mango Flip List that triggers every 3 seconds -->
+  <!--      <div class="list-tile mango">-->
+  <!--          <span class="tile-title">flip list</span>-->
+  <!--          <ul class="flip-list fourTiles" data-mode="flip-list" data-delay="2000">-->
+  <!--              <li>-->
+  <!--                  <div><img class="full" src="/images/sample/1t.gif" alt="1" /></div>-->
+  <!--                  <div><img class="full" src="/images/sample/1tw.gif" alt="1" /></div>-->
+  <!--              </li>-->
+  <!--              <li>-->
+  <!--                  <div><img class="full" src="/images/sample/2t.gif" alt="2" /></div>-->
+  <!--                  <div><img class="full" src="/images/sample/2tw.gif" alt="2" /></div>-->
+  <!--              </li>-->
+  <!--              <li>-->
+  <!--                  <div><img class="full" src="/images/sample/3t.gif" alt="3" /></div>-->
+  <!--                  <div><img class="full" src="/images/sample/3tw.gif" alt="3" /></div>-->
+  <!--              </li>-->
+  <!--              <li data-direction="horizontal">-->
+  <!--                  <div><img class="full" src="/images/sample/4t.gif" alt="4" /></div>-->
+  <!--                  <div><img class="full" src="/images/sample/4tw.gif" alt="4" /></div>-->
+  <!--              </li>-->
+  <!--          </ul>-->
+  <!--      </div>-->
+        <!-- Green Static Tile -->
+  <!--      <div class="live-tile accent green exclude">-->
+  <!--          <span class="tile-title">static tile</span>-->
+  <!--          <p>Static tiles can take advantage of theming too!</p>-->
+  <!--      </div>-->
+  <!--  </div>-->
     <!-- Activate live tiles -->
-    <script type="text/javascript">
+  <!--  <script type="text/javascript">-->
         // apply regular slide universally unless .exclude class is applied 
         // NOTE: The default options for each liveTile are being pulled from the 'data-' attributes
-        $(".live-tile, .flip-list").not(".exclude").liveTile();
-    </script>
-</div>
-
-<?php include("includes/footer.php") ?>
+  <!--      $(".live-tile, .flip-list").not(".exclude").liveTile();-->
+  <!--  </script>  -->
